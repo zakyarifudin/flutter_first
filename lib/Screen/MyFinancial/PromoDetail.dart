e0b21958be438fcc4d35561a28a9f458f5c49e8a
ptksp-zaky <ptksp.zaky@gmail.com>
2020-01-09 11:02:45 +0700

    fix tab bar view jika deskripsi banyak

--- lib/Screen/MyFinancial/PromoDetail.dart
+++ lib/Screen/MyFinancial/PromoDetail.dart
@@ -57,40 +57,47 @@ class _PromoDetail extends State<PromoDetail> with SingleTickerProviderStateMixi
               pinned: true,
               floating: false,
             ),
-            SliverFillRemaining(
-              child: TabBarView(
-                controller: tabController,
-                children: <Widget>[
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
                   Container(
+                    height: 1000,
                     padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: Text(
-                      "Tab Info Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.",
-                      style: TextStyle(
-                        fontSize: 20
-                      ),
-                    )
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black
+                    child: TabBarView(
+                      controller: tabController,
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            "Tab Info Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.",
+                            style: TextStyle(
+                              fontSize: 20
+                            ),
+                          )
                         ),
-                        children: <TextSpan>[
-                          TextSpan(text: 'Tab Cara ', style: TextStyle(fontWeight: FontWeight.bold)),
-                          TextSpan(text: 'merupakan ', style: TextStyle(color: Colors.blue)),
-                          TextSpan(text: 'Tab ', style: TextStyle(color: Colors.red)),
-                          TextSpan(text: 'untuk ', style: TextStyle(color: Colors.green)),
-                          TextSpan(text: 'Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum. Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti "Bagian isi disini, bagian isi disini", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian terhadap kalimat "Lorem Ipsum" akan berujung pada banyak situs web yang masih dalam tahap pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja, kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)')
-                        ],
-                      ),
-                    )
-                  ),
-                ],
+                        Container(
+                          child: RichText(
+                            text: TextSpan(
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(text: 'Tab Cara ', style: TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(text: 'merupakan ', style: TextStyle(color: Colors.blue)),
+                                TextSpan(text: 'Tab ', style: TextStyle(color: Colors.red)),
+                                TextSpan(text: 'untuk ', style: TextStyle(color: Colors.green)),
+                                TextSpan(text: 'Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum. Sudah merupakan fakta bahwa seorang pembaca akan terpengaruh oleh isi tulisan dari sebuah halaman saat ia melihat tata letaknya. Maksud penggunaan Lorem Ipsum adalah karena ia kurang lebih memiliki penyebaran huruf yang normal, ketimbang menggunakan kalimat seperti "Bagian isi disini, bagian isi disini", sehingga ia seolah menjadi naskah Inggris yang bisa dibaca. Banyak paket Desktop Publishing dan editor situs web yang kini menggunakan Lorem Ipsum sebagai contoh teks. Karenanya pencarian terhadap kalimat "Lorem Ipsum" akan berujung pada banyak situs web yang masih dalam tahap pengembangan. Berbagai versi juga telah berubah dari tahun ke tahun, kadang karena tidak sengaja, kadang karena disengaja (misalnya karena dimasukkan unsur humor atau semacamnya)')
+                              ],
+                            ),
+                          )
+                        ),
+                      ],
+                    ),     
+                  )
+                ]
               ),
             ),
+
           ]
         ),
       )
